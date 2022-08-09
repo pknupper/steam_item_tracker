e520cac319bc4ba6076e23af5068a3e22a483082
pknupper <46831211+pknupper@users.noreply.github.com>
2022-08-09 13:32:30 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
+	
+	"github.com/bwmarrin/discordgo"
 )
 
 type Item struct {
 	Name     string
 	HashName string
+	Stock 	 int
 }
 
 type Response struct {
@@ -20,6 +24,16 @@ type Response struct {
 	MedianPrice string `json:"median_price"`
 }
 
+type Post struct {
+}
+
+var (
+	Channel  = flag.String("channel", "", "Channel ID")
+	BotToken = flag.String("token", "", "Bot token")
+)
+
+func init() { flag.Parse() }
+
 func main() {
 	const baseUrl = "https://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name="
 
@@ -27,144 +41,184 @@ func main() {
 
 	var items = []Item{
 		{
+			Name:     "Recoil Case",
+			HashName: "Recoil%20Case",
+			Stock:	  0,
+		},
+		{
 			Name:     "Prisma 2 Case",
 			HashName: "Prisma%202%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Danger Zone Case",
 			HashName: "Danger%20Zone%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Fracture Case",
 			HashName: "Fracture%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Prisma Case",
 			HashName: "Prisma%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Snakebite Case",
 			HashName: "Snakebite%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Horizon Case",
 			HashName: "Horizon%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "CS20 Case",
 			HashName: "CS20%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Revolver Case",
 			HashName: "Revolver%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Shadow Case",
 			HashName: "Shadow%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Falchion Case",
 			HashName: "Falchion%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Clutch Case",
 			HashName: "Clutch%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Chroma 3 Case",
 			HashName: "Chroma%203%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Spectrum 2 Case",
 			HashName: "Spectrum%202%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Gamma Case",
 			HashName: "Gamma%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Chroma 2 Case",
 			HashName: "Chroma%202%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Dreams And Nightmares Case",
 			HashName: "Dreams%20%26%20Nightmares%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Gamma 2 Case",
 			HashName: "Gamma%202%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Operation Vanguard Case",
 			HashName: "Operation%20Vanguard%20Weapon%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Spectrum Case",
 			HashName: "Spectrum%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Chroma Case",
 			HashName: "Chroma%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Shattered Web Case",
 			HashName: "Shattered%20Web%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Operation Phoenix Case",
 			HashName: "Operation%20Phoenix%20Weapon%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Operation Broken Fang Case",
 			HashName: "Operation%20Broken%20Fang%20Casee",
+			Stock:	  0,
 		},
 		{
 			Name:     "Glove Case",
 			HashName: "Glove%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Operation Breakout Case",
 			HashName: "Operation%20Breakout%20Weapon%20Case",
+			Stock:	  0
 		},
 		{
 			Name:     "Huntsman Case",
 			HashName: "Huntsman%20Weapon%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "eSports Summer Case 2014",
 			HashName: "eSports%202014%20Summer%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Winter Offensive Case",
 			HashName: "Winter%20Offensive%20Weapon%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "eSports Winter Case 2013",
 			HashName: "eSports%202013%20Winter%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Weapon Case 3",
 			HashName: "CS%3AGO%20Weapon%20Case%203",
+			Stock:	  0,
 		},
 		{
 			Name:     "Weapon Case 2",
 			HashName: "CS%3AGO%20Weapon%20Case%202",
+			Stock:	  0,
 		},
 		{
 			Name:     "Operation Hydra Case",
 			HashName: "Operation%20Hydra%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "eSports 2013 Case",
 			HashName: "eSports%202013%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Operation Bravo Case",
 			HashName: "Operation%20Bravo%20Case",
+			Stock:	  0,
 		},
 		{
 			Name:     "Weapon Case",
 			HashName: "CS%3AGO%20Weapon%20Case",
+			Stock:	  0,
 		},
 	}
 
@@ -183,9 +237,28 @@ func main() {
 		var response Response
 		json.Unmarshal([]byte(body), &response)
 
-		fmt.Println(item.Name + ": Lowest Price: " + response.LowestPrice + ", Volume: " + response.Volume + ", Median Price: " + response.MedianPrice)
-
 		time.Sleep(5 * time.Second)
 	}
 
+}
+
+func sendDiscordMessage(session *discordgo.Session, baseUrl string, post Post) {
+
+	_, err := session.ChannelMessageSendEmbed(*Channel, &discordgo.MessageEmbed{
+		Description: "hello",
+		Title:       "title",
+		Fields: []&discordgo.MessageEmbedField{
+			{
+				Name: "test1",
+				Value: "test123",
+			},
+			{
+				Name: "test2",
+				Value: "test1234",
+			},
+		},
+	})
+	if err != nil {
+		log.Printf("Error sending message: %v", err)
+	}
 }
